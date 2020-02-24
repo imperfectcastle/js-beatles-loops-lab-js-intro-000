e4982dd2f6a6e215293555c6fdc93e2958cc7e3a
Bart Sakowski <bart.sakowski@gmail.com>
2020-02-24 04:06:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,8 +27,7 @@ function johnLennonFacts(array) {
 function iLoveTheBeatles(num){
   var love = []
   do {love.push("I love The Beatles!");
-} while (iLoveTheBeatles() < 15) {
-  num++ ;
-};
+  num++ 
+} while (iLoveTheBeatles() < 15)
 return(love)
 }
