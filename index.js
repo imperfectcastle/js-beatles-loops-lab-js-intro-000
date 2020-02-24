b81da9f50ed1efdf2573856b504c734391a357f0
Bart Sakowski <bart.sakowski@gmail.com>
2020-02-24 03:32:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// add solution here
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments = ["Guitar", "Bass guitar", "Lead Guitar", "Drums"];
+var answer = []
+
+function theBeatlesPlay(musicians, instruments) {
+  for (var i = 0; i < 4; i++){
+    answer.push(`${musicians[i]} plays ${instruments[i]}`);
+    }
+  return(answer)
+}
+
+///code above is correct
+
+
+
+function johnLennonFacts(array) {
+  var i = 0 ;
+  var funfacts = []
+  while (i < array.length) {
+  funfacts.push(array[i] + "!!!");
+  i++; }
+  return(funfacts)
+}
